560186f7b1bc2c820f95812ffda678c2f3809c18
John <Xzten3@gmail.com>
2020-06-01 23:17:49 -0600

    QL Approach (best: -177)

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 import gym
-from rlfam import Printer, RL
-
-env = gym.make('LunarLander-v2')
-env.reset()
+from rlfam import Printer, QL
 
 
-def run(epochs=10):
+def run(epochs=1000):
     printer = Printer()
-    for epoch in range(epochs):
-        print("EPOCH ------> {}".format(epoch+1))
-        done = False
-        totalReward = 0
-        while not done:
-            env.render()
-            action = env.action_space.sample()  # Random Action
-            obs, reward, done, info = env.step(action)
-            totalReward += reward
+    ql = QL(state_space=8, action_space=env.action_space.n)
 
-            # printer.printState(obs, reward, done, info)
+    obs = env.reset()
+    for epoch in range(epochs):
+        if (epoch + 1) % (epochs / 10) == 0:
+            print("EPOCH ------> {}".format(epoch+1))
+
+        done = False
+        total_reward = 0
+        while not done:
+            if (epoch + 1) % (epochs / 10) == 0:
+                env.render()
+            action = ql.take_action(obs)
+            new_obs, reward, done, info = env.step(action)
+
+            ql.update(action, obs, new_obs, reward)
+            obs = new_obs
+
+            total_reward += reward
 
             if done:
                 obs = env.reset()
-        printer.printResults(totalReward)
-        env.close()
+
+        printer.rewards.append(total_reward)
+        if (epoch + 1) % (epochs / 10) == 0:
+            ql.explore -= .01
+
+            if ql.explore < .01: ql.explore = .01
+
+            printer.print_results()
+    env.close()
 
 
 if __name__ == "__main__":
+    env = gym.make('LunarLander-v2')
     run()
--- rlfam.py
+++ rlfam.py
@@ -1,8 +1,57 @@
 import numpy as np
+import random
 
-class RL(object):
-    def __init__(self):
-        pass
+
+class QL(object):
+    Q = []
+    state_space = None
+    action_space = None
+    groups = None
+    alpha = 1.0
+    gamma = 1.0
+    explore = 1.0
+
+    def __init__(self, state_space, action_space, group=5):
+        self.state_space = state_space
+        self.action_space = action_space
+        self.groups = group
+
+        shap = [self.groups for i in range(self.state_space)]
+        shap.append(self.action_space)
+        shap = tuple(shap)
+
+        self.Q = np.zeros(shape=shap)
+
+    def continous_2_descrete(self, obs):
+        """
+            Convert Observation to Discrete
+        :param obs: env state
+        :return: transformed obs
+        """
+        location = self.Q
+        for i in range(len(obs)):
+            value = int(obs[i] * 100) % self.groups
+            location = location[value]
+
+        return location
+
+    def take_action(self, obs):
+        """
+            Explore if random number is greater than #
+            Otherwise, return action with highest known reward
+        :param obs: env state converted to 0 or 1 then summed as the Q state index
+        :return: action
+        """
+        if random.random() < self.explore:
+            return np.random.randint(0, self.action_space)
+        return np.argmax(self.continous_2_descrete(obs))
+
+    def update(self, action, obs, next_obs, reward):
+        obs = self.continous_2_descrete(obs)
+        next_obs = self.continous_2_descrete(next_obs)
+
+        next_obs[action] = (1 - self.alpha) * obs[action] \
+            + self.alpha * (reward + self.gamma * max(next_obs))
 
 
 class Printer(object):
@@ -11,7 +60,7 @@ class Printer(object):
     def __init__(self):
         pass
 
-    def printState(self, obs, reward, done, info):
+    def print_state(self, obs, reward, done, info):
         print("\nState --->")
         print("   X Position: {}".format(obs[0]))
         print("   Y Position: {}".format(obs[1]))
@@ -27,9 +76,5 @@ class Printer(object):
         if info:
             print("Info: {}".format(info))
 
-    def printResults(self, totalReward):
-        print("---> Total Reward: {}".format(round(totalReward, 3)))
-        if len(self.rewards) > 2:
-            print("---> Improvement: {}%".format(round(100 * (totalReward / self.rewards[-1] - 1), 3)))
-        self.rewards.append(totalReward)
+    def print_results(self):
         print("---> Avg Reward: {}\n".format(round(np.average(self.rewards), 3)))
